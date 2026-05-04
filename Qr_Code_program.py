2d35b24df39fd1f0042dcea36fb207a20439ac97
Varnitha-40 <sreevarnitham@gmail.com>
2026-05-04 15:56:19 +0530

    Create Qr_Code_program.py

---
+++ Qr_Code_program.py
@@ -0,0 +1,9 @@
+#create qr code
+import qrcode
+qr=qrcode.QRCode()
+text="TVK Is HEADING towards CHENNAI HEADQUERTERS"
+qr.add_data(text)
+qr.make(fit=True)
+res=qr,make_image(fil_color="black",back_color="white")
+res.save("NIHARIKA.png")
+print("created niha")
